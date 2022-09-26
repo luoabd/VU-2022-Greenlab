b1c17a579c95398f53ff5dd44bcfec7b8655b071
Ruben Horn <>
2022-09-26 12:40:29 +0200

    Updated app versions

--- utils/generate_summary.sh
+++ utils/generate_summary.sh
@@ -10,7 +10,7 @@ echo -n "Generating $(pwd)/summary.txt..."
 
 for app in $(ls | grep ".apk$")
 do
-    echo "[$app ($(md5sum $app | awk '{ print $1 }'))]" >> summary.txt
+    echo "[$app (md5sum=$(md5sum $app | awk '{ print $1 }'))]" >> summary.txt
     aapt dump badging $app 2>/dev/null | head -n 1 >> summary.txt
     echo "" >> summary.txt
 done
