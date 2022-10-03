61bdbf53eed7b1567dd5379f4b1ac723e81d23a9
Ruben Horn <>
2022-10-03 15:52:10 +0200

    interaction twitch app

--- experiments/main/Scripts/interactions/tv_twitch_android_app.sh
+++ experiments/main/Scripts/interactions/tv_twitch_android_app.sh
@@ -1,21 +1,13 @@
 #! /bin/bash
 
-# precondition: twitch.tv is already opened in the browser
+# precondition: Twitch app is already opened (logged in, dismissed initial pop-ups)
 # postcondition: live stream is playing (24/7)
 
 # Check device resolution
 test "$(adb shell wm size | grep -oP "\d.*$")" == "1080x2340" || echo "Warning! Expected 1080x2340 device."
 
-# Wait for any cookie pop-up to appear
-sleep 3
-# Accept cookies
-adb shell input tap 750 1450
-
-# Go back just in case we did just open a video (no cookie pop-up)
-adb shell input tap 50 150
-
 # Open search
-adb shell input tap 900 150
+adb shell input tap 950 2250
 sleep 1
 # Look up 24/7 stream (animals)
 adb shell input text 'CritterVision'
