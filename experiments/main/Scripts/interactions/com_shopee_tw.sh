db027e4a165319caed9478781710135901a70564
luoabd <abdellah.lahnaoui@gmail.com>
2022-10-06 01:57:42 +0200

    Improved Shopee app interaction

--- experiments/main/Scripts/interactions/com_shopee_tw.sh
+++ experiments/main/Scripts/interactions/com_shopee_tw.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
-# precondition: Shopee app is already opened in the browser
+# precondition: Shopee app is already opened
 # postcondition: 
 
 # Check device resolution
 test "$(adb shell wm size | grep -oP "\d.*$")" == "1080x2340" || echo "Warning! Expected 1080x2340 device."
 
 START=$(date +%s)
+init=1
+
+# TODO: Adjust values for test device
 
 # Wait for the initial ad
 sleep 13
@@ -21,13 +24,16 @@ sleep 1
 while [ $(($(date +%s) - $START)) -lt 320 ] # Leave about 5 seconds slack for the last interaction
 do
     # Main interaction loop (takes roughly 35 seconds)
-    # Scroll down
-    adb shell input swipe 500 1300 500 250
-    sleep 1
-    adb shell input swipe 500 1300 500 250
-    sleep 1
-    adb shell input swipe 500 1300 500 250
-    sleep 3
+    # Skip the multiple promotions
+    if [ $init == 1 ]; then
+        adb shell input swipe 500 1300 500 250
+        sleep 1
+        adb shell input swipe 500 1300 500 250
+        sleep 1
+        adb shell input swipe 500 1300 500 250
+        sleep 1
+    fi
+    let "init=0" 
 
     # Open product
     adb shell input tap 150 375
@@ -50,6 +56,11 @@ do
     sleep 2
 
     # Go back to the previous page
-    adb shell input tap 30 90
+    adb shell input keyevent 4
     sleep 3
+
+    # Scroll down 
+    adb shell input swipe 500 1300 500 250
+    sleep 2
+
 done
