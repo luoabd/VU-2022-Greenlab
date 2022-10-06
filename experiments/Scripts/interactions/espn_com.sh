ffb8cfb684034239820500609e6f3eefcf63d577
luoabd <abdellah.lahnaoui@gmail.com>
2022-10-07 01:49:57 +0200

    Interaction for espn.com

---
+++ experiments/Scripts/interactions/espn_com.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# precondition: espn.com is already opened in the browser
+# postcondition: 
+
+# Check device resolution
+test "$(adb shell wm size | grep -oP "\d.*$")" == "1080x2340" || echo "Warning! Expected 1080x2340 device."
+
+distance=60 # Distance between headlines
+loc=790
+
+while true
+do
+    for i in {0..7}
+    do
+        # Main interaction loop
+        # Scroll down (to top headlines)
+        adb shell input swipe 500 2000 500 50
+        sleep 1
+
+        # Open article
+        let "loc+=($distance*$i)"
+        adb shell input tap 130 $loc
+        sleep 2
+
+        # Scroll down
+        adb shell input swipe 500 1300 500 250
+        sleep 1
+        adb shell input swipe 500 1300 500 250
+        sleep 2
+
+        # Go back to the previous page
+        adb shell input keyevent 4
+        sleep 3
+    done
+
+done
